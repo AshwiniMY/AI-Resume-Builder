3419b48f13c6507c19658dab20db6ceb60979269
ashwini09 <ashwinimyaraguppi@gmail.com>
2026-05-08 22:10:12 +0530

    Switch to HashRouter to permanently fix 404 on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import SignInPage from './auth/sign-in/index.jsx'
 import Home from './home/index.jsx'
 import Dashboard from './dashboard/index.jsx'
@@ -11,7 +11,7 @@ import EditResume from './dashboard/resume/[resumeId]/edit/index.jsx'
 import ViewResume from './my-resume/[resumeId]/view/index.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-const router=createBrowserRouter([
+const router=createHashRouter([
   {
     path:'/',
     element:<Home/>
